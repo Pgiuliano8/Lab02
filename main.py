499ade799e220223447cee601be60b119477b5a2
Pgiuliano8 <s310899@studenti.polito.it>
2025-03-06 18:52:09 +0100

    implementati metodi

--- main.py
+++ main.py
@@ -7,19 +7,30 @@ while(True):
 
     t.printMenu()
 
-    t.loadDictionary("filename.txt")
+    t.loadDictionary("dictionary.txt")
 
     txtIn = input()
 
     # Add input control here!
 
     if int(txtIn) == 1:
-        print()
-        txtIn = input()
-        pass
+        print("Ok, quale parola devo aggiungere?")
+        parola = input()
+        t.addDicLine(parola.lower())
+        print("Parola aggiunta correctamente")
     if int(txtIn) == 2:
-        pass
+        print("Ok, quale parola devo tradurre?")
+        parolaAliena = input()
+        traduzione = t.dizionario.get(parolaAliena.lower())
+        print(traduzione)
+        break
     if int(txtIn) == 3:
-        pass
+        break
     if int(txtIn) == 4:
+        print("Ok, stampo tutto il dizionario")
+        for chiave, valore in t.dizionario.items():
+            print(f"{chiave}: {valore}")
+        break
+    if int(txtIn) == 5:
+        print("Exit")
         break
--- translator.py
+++ translator.py
@@ -1,18 +1,30 @@
 class Translator:
 
     def __init__(self):
-        pass
+        self.dizionario = {}
 
     def printMenu(self):
-        # 1. Aggiungi nuova parola
-        # 2. Cerca una traduzione
-        # 3. Cerca con wildcard
-        # 4. Exit
-        pass
+        print("------------------------------")
+        print("   Translator Alien-Italian   ")
+        print("------------------------------")
+        print("\n1. Aggiungi nuova parola\n2. Cerca una traduzione\n3. Cerca con wildcard\n4. Stampa tutto il dizionario\n"
+              "5. Exit\n")
+
 
     def loadDictionary(self, dict):
         # dict is a string with the filename of the dictionary
-        pass
+        with open(dict, 'r', encoding='utf-8') as f:
+            for riga in f:
+                parole = riga.strip().split(" ")
+                self.dizionario[parole[0]] = parole[1]
+
+    def addDicLine(self, entry):
+        campi = entry.split(" ")
+        self.dizionario[campi[0]] = campi[1]
+
+
+
+
 
     def handleAdd(self, entry):
         # entry is a tuple <parola_aliena> <traduzione1 traduzione2 ...>
